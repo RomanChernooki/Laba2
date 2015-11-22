f97bf3ddf72b636f09d45236ea361fce5391e7f3
RomanChernooki <bromanwot@gmail.com>
2015-11-22 22:48:33 +0300

    Создал класс Фильм.

--- filmlister/film.h
+++ filmlister/film.h
@@ -1,11 +1,20 @@
 #ifndef FILM_H
 #define FILM_H
+#include <QList>
+#include <QString>
+#include <QDate>
 
 
 class Film
 {
 public:
     Film();
+    QString name;
+    QList<QString> actors;
+    QString director;
+    QString description;
+    QDate date;
+    int rating;
 };
 
 #endif // FILM_H
