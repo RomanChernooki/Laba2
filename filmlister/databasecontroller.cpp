6b4fa9a1e4f8ab2fd71271850bb514183288382d
RomanChernooki <bromanwot@gmail.com>
2015-11-30 19:18:36 +0300

    Допилил полный функционал базы данных.

--- filmlister/databasecontroller.cpp
+++ filmlister/databasecontroller.cpp
@@ -9,7 +9,13 @@ DatabaseController::DatabaseController()
         qDebug()<<db.lastError();
      QSqlQuery query(db);
      query.exec("CREATE TABLE IF NOT EXISTS film (id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                    "name VARCHAR(20))");
+                    "name TEXT,"
+                "director TEXT,"
+                "actors TEXT,"
+                "description TEXT,"
+                "date TEXT,"
+                "genre TEXT,"
+                "rating TEXT)");
      qDebug() << query.lastError();
         if(!query.exec("SELECT * FROM film"))
              qDebug()<<query.lastError();
@@ -17,6 +23,12 @@ DatabaseController::DatabaseController()
          Film f;
             f.name = query.value(1).toString();
             f.id = query.value(0).toInt();
+            f.director= query.value(2).toString();
+            f.actors=query.value(3).toString();
+            f.description=query.value(4).toString();
+            f.date=query.value(5).toString();
+            f.genre=query.value(6).toString();
+            f.rating=query.value(7).toString();
             filmList.push_back(f);
             qDebug() << f.name << f.id;
         }
@@ -27,7 +39,8 @@ void DatabaseController::addFilm(Film film)
 {
     filmList.push_back(film);
     QSqlQuery query(db);
-        if(!query.exec("INSERT INTO film (id, name) "
-                   "VALUES ("+QString::number(film.id)+", '"+film.name+"')"))
+        if(!query.exec("INSERT INTO film (id, name, director, actors, description, date, genre, rating) "
+                   "VALUES ("+QString::number(film.id)+", '"+film.name+"', '"+film.director+"', '"+film.actors+"', '"+film.description
+                       +"', '"+film.date+"', '"+film.genre+"', '"+film.rating+"')"))
             qDebug()<<query.lastError();
 }
